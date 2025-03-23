4b0a2eb2671f24b55aeb5b72615eaf3d7622b6b4
zannatulferdousianika <zfanika79@gmail.com>
2025-03-23 22:44:24 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded" , ()=>{
+
+      const diceImages = document.getElementById("diceImage");
+      const rollBtn = document.getElementById("rollBtn");
+
+
+      const diceImage = [
+        
+        "dice-1.png",
+        "dice-2.png",
+        "dice-3.png",
+        "dice-4.png",
+        "dice-5.png",
+        "dice-6.png",
+      ];
+
+      function rollBtn(){
+        const randomIndex =  Math.floor(Math.random() * diceImage.length);
+
+        diceImage.style.animation = "roll 1s ease-in-out"
+        setTimeout(()=>{
+            diceImage.src = diceImage(randomIndex);
+            diceImage.style.animation = "";
+
+        }, 1000)
+      }
+      rollBtn.addEventListener("click", rollDice);
+});
